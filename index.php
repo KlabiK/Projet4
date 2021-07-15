d23d6ffa9e91f7d1e5dddb90a0e1380802cd8e14
KlabiK <49153337+KlabiK@users.noreply.github.com>
2021-07-15 13:39:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,36 +1,107 @@
 <?php
-require('controller/frontend.php');
+ if(session_status() == PHP_SESSION_NONE){
+    session_start();
+  }
+require_once('config/functions.php');
 
-try {
-    if (isset($_GET['action'])) {
-        if ($_GET['action'] == 'listPosts') {
-            listPosts();
-        }
-        elseif ($_GET['action'] == 'post') {
-            if (isset($_GET['id']) && $_GET['id'] > 0) {
-                post();
-            }
-            else {
-                throw new Exception('Aucun identifiant de billet envoyé');
-            }
-        }
-        elseif ($_GET['action'] == 'addComment'){
-            if (isset($_GET['id']) && $_GET['id'] > 0){
-                if (!empty($_POST['auteur']) && !empty($_POST['commentaire'])){
-                    addComment($_GET['id'], $_POST['auteur'], $_POST['commentaire']);
+$articles = getArticles();
+?>
+
+<!DOCTYPE html>
+    <html>
+        <head>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+            <meta charset="UTF-8">
+            <title>Premier blog</title>
+        </head>
+        <body>
+             <div class="bg-dark">
+          <div class ="container">
+              <div class="row">      
+                <nav class="col navbar navbar-expand-lg navbar-dark">
+                   <a class="navbar-brand" href="index.php">Jean Forteroche</a>
+                    <div id="navbarContent" class="collapse navbar-collapse">
+                       <ul class="navbar-nav">
+                          <li class="nav-item active">
+                             <a class="nav-link" href="index.php">Accueil</a>
+                          </li>
+                          <li class="nav-item">
+                             <a class="nav-link" href="articlesList.php">Chapitres</a>
+                          </li>
+                          <li class="nav-item">
+                             <a class="nav-link" href="contact.php">Contact</a>
+                          </li>
+                       </ul>
+                   </div>
+                </nav>
+            </div>
+        </div>
+      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+            <?php
+                 if(!empty($_SESSION['message'])){
+                    echo '<div class="alert alert-success" role="alert">'. $_SESSION['message'].'</div';
+                    $_SESSION['message'] = "";
+                 }
+                if(!empty($_SESSION['erreur'])){
+                        echo '<div class="alert alert-danger" role="alert">'. $_SESSION['erreur'].'</div';
+                        $_SESSION['erreur'] = "";
                 }
-                else{
-                    throw new Exception(' Tous les champs ne sont pas remplis !');
-                }
-            }else{
-                throw new Exception ('Aucun identifiant de billet envoyé');
-            }
-        }
-    }
-    else {
-        listPosts();
-    }
-}
-catch(Exception $e) {
-    echo 'Erreur : ' . $e->getMessage();
-}
+                ?>
+            </div>
+        </div>
+    </div>
+            <div class="container">
+            <h2 class="text-center my-4">Biographie </h2>
+                <div class="row">
+                    <div class="col-4 my-4 mx-4">
+                    <img src="photo01.jpg" alt="Bootstrap" class="img-rounded" width="120%">
+                    </div>
+                    <div class="col-6 my-4 mx-5">
+                        <p>Saepissime igitur mihi de amicitia cogitanti maxime illud considerandum videri solet, utrum propter imbecillitatem atque inopiam desiderata sit amicitia, ut dandis recipiendisque meritis quod quisque minus per se ipse posset, id acciperet ab alio vicissimque redderet, an esset hoc quidem proprium amicitiae, sed antiquior et pulchrior et magis a natura ipsa profecta alia causa. Amor enim, ex quo amicitia nominata est, princeps est ad benevolentiam coniungendam. Nam utilitates quidem etiam ab iis percipiuntur saepe qui simulatione amicitiae coluntur et observantur temporis causa, in amicitia autem nihil fictum est, nihil simulatum et, quidquid est, id est verum et voluntarium.</p>
+                    </div>
+                </div>
+
+            </div>
+    </div>
+    <div class="container">
+            <h2 class="text-center">Oeuvres</h2>
+                <div class="row">
+
+                    <div class="col my-4">
+                        <p>Saepissime igitur mihi de amicitia cogitanti maxime illud considerandum videri solet, utrum propter imbecillitatem atque inSaepissime igitur mihi de amicitia cogitanti maxime illud considerandum videri solet, utrum propter imbecillitatem atque inopiam desSaepissime igitur mihi de amicitia cogitanti maxime illud considerandum videri solet, utrum propter imbecillitatem atque inopiam desopiam desiderata sit amicitia, ut dandis recipiendisque meritis quod quisque minus per se ipse posset, id acciperet ab alio vicissimque redderet, an esset hoc quidem proprium amicitiae, sed antiquior et pulchrior et magis a natura ipsa profecta alia causa. Amor enim, ex quo amicitia nominata est, princeps est ad benevolentiam coniungendam. Nam utilitates quidem etiam ab iis percipiuntur saepe qui simulatione amicitiae coluntur et observantur temporis causa, in amicitia autem nihil fictum est, nihil simulatum et, quidquid est, id est verum et voluntarium.</p>
+                    </div>
+                </div>
+
+            </div>
+    </div>
+    <div class="container">
+            <h2 class="text-center">Billet simple pour l'Alaska </h2>
+                <div class="row">
+                    <div class="col-6 my-4 mx-4">
+                        <p>Saepissime igitur mihi de amicitia cogitanti maxime illud considerandum videri solet, utrum propter imbecillitatem atque inopiam desiderata sit amicitia, ut dandis recipiendisque meritis quod quisque minus per se ipse posset, id acciperet ab alio vicissimque redderet, an esset hoc quidem proprium amicitiae, sed antiquior et pulchrior et magis a natura ipsa profecta alia causa. Amor enim, ex quo amicitia nominata est, princeps est ad benevolentiam coniungendam. Nam utilitates quidem etiam ab iis percipiuntur saepe qui simulatione amicitiae coluntur et observantur temporis causa, in amicitia autem nihil fictum est, nihil simulatum et, quidquid est, id est verum et voluntarium.</p>
+                    </div>
+                    <div class="col-4 my-4 mx-4">
+                       <img src="Alaska.jpg" alt="Bootstrap" class="img-rounded" width="120%">
+                    </div>
+
+                </div>
+    </div>
+<div class="jumbotron">
+   <div class="container">
+      <div class="row">
+         <div class="col text-center ">
+            <ul class="list-inline ">
+               <li class="list-inline-item font-weight-bold">À propos</li>
+               <li class="list-inline-item font-weight-bold"> Vie privée</li>
+               <li class="list-inline-item font-weight-bold">Conditions d'utilisations</li>
+               <li class="list-inline-item font-weight-bold"><a href="login.php">Admin</a></li>
+            </ul>
+         </div>     
+      </div>
+   </div>
+</div>
+        </body>
+    </html>
